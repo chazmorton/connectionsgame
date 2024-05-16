93ae3d70f94e60be5ff4b2cb481a844e84695c78
Chaz Morton <chazmortonjr@gmail.com>
2024-05-15 21:11:41 -0600

    additional styling changes

--- src/api/getGameDayData.js
+++ src/api/getGameDayData.js
@@ -4,7 +4,7 @@ async function getGameDayData() {
 
         const axios = require('axios');
 
-        const response = await axios.get('https://www.nytimes.com/svc/connections/v2/2024-05-05.json');
+        const response = await axios.get('https://www.nytimes.com/svc/connections/v2/2024-05-15.json');
 
         console.log(response.data);
 
